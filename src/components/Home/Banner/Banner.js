cb222c45e94804bd82d6b71fb347d112525ca8b8
Palak-SweetCoder <pspalak6@gmail.com>
2022-11-06 19:24:34 +0600

    banner done with mobile and tablet responsive

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Banner.css';
+import banner from '../../../assets/electromax-banner.png';
 
 const Banner = () => {
     return (
-        <div className="bg-warning">
-            <h1>Here will be the banner</h1>
+        <div className="banner">
+            <div className="container d-flex align-items-center justify-content-between banner-container">
+                <div className="p-lg-5">
+                    <h1>Tempor incididunt ut labore et dolor.</h1>
+                    <p>
+                        It is a long established fact that a reader will be
+                        distracted by the readable content of a page when
+                        looking at its layout. The point of using Lorem Ipsum is
+                        that it has a more-or-less normal
+                    </p>
+                </div>
+                <div className="p-lg-2">
+                    <img src={banner} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
