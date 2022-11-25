072a741cf49ab6dc6c0801734e8bc36c6dc3bf66
Palak-SweetCoder <pspalak6@gmail.com>
2022-11-25 23:21:05 +0600

    env file for firebase done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyCCpuC7yhf9lWbHJsIKExyzEAtCXyz0s5s',
-    authDomain: 'electro-max.firebaseapp.com',
-    projectId: 'electro-max',
-    storageBucket: 'electro-max.appspot.com',
-    messagingSenderId: '421630770490',
-    appId: '1:421630770490:web:5d841f70a70ea5c0c8e403',
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
