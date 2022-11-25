6a0ebe499bbb17c7cb537e42790c724384f53cb0
Palak-SweetCoder <pspalak6@gmail.com>
2022-11-25 23:27:22 +0600

    requireauth reloading redirect problem solved

--- src/components/Login/RequireAuth/RequireAuth.js
+++ src/components/Login/RequireAuth/RequireAuth.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const location = useLocation();
+    if(loading){
+        return <div className='p-5 text-center'><Loading></Loading></div>
+    }
     if (!user) {
         return (
             <Navigate
