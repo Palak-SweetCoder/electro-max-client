13848333cb8bed65c40422ad78d206dda040fccd
Palak-SweetCoder <pspalak6@gmail.com>
2022-12-13 23:04:24 +0600

    inventory card design with responsive

--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -13,9 +13,33 @@ const InventoryItems = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Here is your inventory item: {item.name}</h1>
-        </div>
+        <>
+            <div className="container mb-5 mt-5">
+                <div className="card w-75 p-2 mx-auto">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img
+                                src={item.img}
+                                className="img-fluid rounded-start"
+                                alt="..."
+                            />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h4 className="card-title">{item.name}</h4>
+                                <p className="card-text">{item.description}</p>
+                                <p>Price: ${item.price}</p>
+                                <p>Supplier: {item.supplier}</p>
+                                <p>quantity: {item.quantity}</p>
+                                <button className="item-button rounded-pill px-3 py-2">
+                                    Delivered
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
