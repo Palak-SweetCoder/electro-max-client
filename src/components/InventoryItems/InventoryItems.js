f9e3edb1bf17114ff1c6e4dc8fe086e4be978632
Palak-SweetCoder <pspalak6@gmail.com>
2022-12-27 19:54:15 +0600

    update quantity by clicking delivered and update data to the server done

--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -4,13 +4,33 @@ import { useParams } from 'react-router-dom';
 const InventoryItems = () => {
     const { itemsId } = useParams();
     const [item, setItem] = useState({});
+    const { img, price, name, supplier, description, quantity } = item;
 
     useEffect(() => {
         const url = `http://localhost:5000/items/${itemsId}`;
         fetch(url)
             .then((res) => res.json())
-            .then((data) => setItem(data));
-    }, []);
+            .then((items) => setItem(items));
+    }, [item]);
+
+    const reduceQuantity = () => {
+        const productQuantity = { quantity: quantity - 1 };
+
+        // send data to the server side
+        const url = `http://localhost:5000/items/${itemsId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(productQuantity),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setItem(data);
+                alert('Quantity updated!!!');
+            });
+    };
 
     return (
         <>
@@ -19,19 +39,22 @@ const InventoryItems = () => {
                     <div className="row g-0">
                         <div className="col-md-4">
                             <img
-                                src={item.img}
+                                src={img}
                                 className="img-fluid rounded-start"
                                 alt="..."
                             />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h4 className="card-title">{item.name}</h4>
-                                <p className="card-text">{item.description}</p>
-                                <p>Price: ${item.price}</p>
-                                <p>Supplier: {item.supplier}</p>
-                                <p>quantity: {item.quantity}</p>
-                                <button className="item-button rounded-pill px-3 py-2">
+                                <h4 className="card-title">{name}</h4>
+                                <p className="card-text">{description}</p>
+                                <p>Price: ${price}</p>
+                                <p>Supplier: {supplier}</p>
+                                <p>quantity: {quantity}</p>
+                                <button
+                                    onClick={reduceQuantity}
+                                    className="item-button rounded-pill px-3 py-2"
+                                >
                                     Delivered
                                 </button>
                             </div>
