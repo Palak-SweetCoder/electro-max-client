879cbb794348b932bbe7a88a07a7c6e398963642
Palak-SweetCoder <pspalak6@gmail.com>
2022-12-30 23:46:45 +0600

    footer updated

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,17 +1,87 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { ImFacebook, ImInstagram, ImLinkedin, ImTwitter } from 'react-icons/im';
 import './Footer.css';
 
 const Footer = () => {
     return (
         <>
             <footer className="footer-container">
-                <div className="container p-5">
-                    <div className="p-2 text-center">
-                        <h3 className="footer-title">ELECTRO MAX</h3>
+                <div className="container p-5 d-flex justify-content-between flex-wrap">
+                    <div className="p-2">
+                        <h3 className="footer-title">CONTACT US</h3>
+                        <p>
+                            Email: electromaxhelp@gmail.com <br />
+                            Cell: +1-412-496-2290
+                        </p>
                         <p>
                             &copy; COPYRIGHT ELECTRO MAX{' '}
                             {new Date().getFullYear()}
                         </p>
+                    </div>
+
+                    {/* company section  */}
+                    <div className="d-flex flex-column align-items-baseline p-2">
+                        <h3 className="footer-title">COMPANY</h3>
+                        <Button
+                            variant="link"
+                            className="p-0 m-0 text-decoration-none btn-color"
+                        >
+                            About Us
+                        </Button>
+                        <Button
+                            variant="link"
+                            className="p-0 m-0 text-decoration-none btn-color"
+                        >
+                            Privacy Policy
+                        </Button>
+                        <Button
+                            variant="link"
+                            className="p-0 m-0 text-decoration-none btn-color"
+                        >
+                            Terms & Condition
+                        </Button>
+                        <Button
+                            variant="link"
+                            className="p-0 m-0 text-decoration-none btn-color"
+                        >
+                            Legal
+                        </Button>
+                    </div>
+
+                    {/* Follow us section  */}
+                    <div className="p-2">
+                        <h3 className="footer-title">FOLLOW US ON</h3>
+                        <div className="">
+                            <a
+                                href="https://facebook.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <ImFacebook className="icon-width me-3" />
+                            </a>
+                            <a
+                                href="https://twitter.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <ImTwitter className="icon-width me-3" />
+                            </a>
+                            <a
+                                href="https://instagram.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <ImInstagram className="icon-width me-3" />
+                            </a>
+                            <a
+                                href="https://linkedin.com"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <ImLinkedin className="icon-width me-3" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </footer>
